ae8c15381ceff2cc3ceeeed348ce11e8e54e514b
AstHrO <thomas@asthrolab.fr>
2016-02-17 17:32:26 +0100

    Adding time & text functions

---
+++ font_diagonal.h
@@ -0,0 +1,37 @@
+/*
+ * font_diagonal_cw.h
+ *
+ */
+
+/**********************************************************************************
+Description:    Diagonal font
+Author:       Frank Andre / Thomas Hocedez
+Copyright 2015 / 2016:   Frank Andre  / Thomas Hocedez
+License:      see "license.md"
+Disclaimer:     This software is provided by the copyright holder "as is" and any 
+          express or implied warranties, including, but not limited to, the 
+          implied warranties of merchantability and fitness for a particular 
+          purpose are disclaimed. In no event shall the copyright owner or 
+          contributors be liable for any direct, indirect, incidental, 
+          special, exemplary, or consequential damages (including, but not 
+          limited to, procurement of substitute goods or services; loss of 
+          use, data, or profits; or business interruption) however caused 
+          and on any theory of liability, whether in contract, strict 
+          liability, or tort (including negligence or otherwise) arising 
+          in any way out of the use of this software, even if advised of 
+          the possibility of such damage.
+          
+**********************************************************************************/
+const  uint8_t font[] PROGMEM = {
+0x00, 0x38, 0x44, 0x42, 0x42, 0x22, 0x1C, 0x00, // 0
+0x00, 0x20, 0x40, 0xA0, 0x14, 0x0C, 0x04, 0x00,// 1
+0x10, 0x28, 0x48, 0x88, 0x11, 0x22, 0x1C, 0x00, // 2
+0x10, 0x20, 0x40, 0x48, 0x31, 0x12, 0x0C, 0x00, // 3
+0x00, 0x00, 0x44, 0x2A, 0x11, 0x28, 0x04, 0x00, // 4
+0x00, 0x30, 0x40, 0x46, 0x49, 0x32, 0x04, 0x08, // 5
+0x00, 0x38, 0x44, 0x46, 0x4A, 0x32, 0x04, 0x08, // 6
+0x00, 0x00, 0x40, 0x20, 0x11, 0x12, 0x14, 0x08, // 7
+0x00, 0x38, 0x48, 0x4E, 0x72, 0x12, 0x1C, 0x00, // 8
+0x00, 0x30, 0x40, 0x4E, 0x52, 0x22, 0x1C, 0x00}; // 9
+
+
--- sandclock.ino
+++ sandclock.ino
@@ -1,23 +1,19 @@
-#include <Adafruit_GFX.h>
-#include <Max72xxPanel.h>
-
-
 // Sandclock for Arduino
-// V1.2 -  february 2016
+// V1.6 -  february 2016
 // Blenderlab (http://www.blenderlab.fr / http://www.asthrolab.fr)
 // Fab4U (http://www.fab4U.de)
-
-
+//
 // Hardware needed :
 // Arduino (Uno,Mini,Micro...)
 // 2 Led Matrix with MAX7219 controller
+// DS103 RTC for realtime &keeping time in memory.
 // Wires, powersupply ...;-D
-
+//
+//
 // Connect the first matrix to the arduino (5 pins, as follow)
 // Connect (chain) the second matrix to the first one : All same pins except (DOUT->DIN)
 // Taht's it !
-
-
+//
 // Matrix pinout
 // CS = 10
 // CL = 13
@@ -25,27 +21,38 @@
 //
 // SW350D : 12 & GND (Vibration sensor) - (We use internal pullup)
 
+
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
+#include "font_diagonal.h";
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include <Wire.h>
+#include "RTClib.h"
+
 #define HAUT 0
 #define BAS 1
 #define ON 1
 #define OFF 0
-#define SHAKE 12
+#define SHAKE 12 // Vibration sensor pin
 
-String tape = "Arduino";
+#define MODE 2
+// 1 = normal - random length loop
+// 2 = 1 minute per loop.
+
 int wait = 20; // In milliseconds
-
-int ln = 5;
-
-
+int ln = 5; // Filling level of the globe
 int pinCS = 10;
+// One second between each marble drop :
+int htimeout = 1000;
+  
 Max72xxPanel matrix = Max72xxPanel(pinCS, 1, 2);
-
+RTC_Millis rtc;
 
 // Timer for benchmarking
 unsigned long startms = millis();
 
 // Variable for the timing :
-int timer = 0;
+unsigned long timer = 0;
 
 //  Timeout for a next-grain drop
 int next_grain = 0;
@@ -65,26 +72,53 @@ void cleanup_pixels() {
 // Filling the Upper glass with some sand :
 void fill_hourglass(int niv = 5, int globe = HAUT) {
   cleanup_pixels();
+  byte minu = 0;
+  niv = 0;
+
+#if MODE==1
+
   for (int y = 0; y < 8; y++) {
     for (int x = 0; x < 8; x++) {
       if ((x + y) > niv) {
         set_pixel(x, y, globe, ON);
         draw_hourglass();
-        delay(30);
+        minu++;
+        delay(10);
       }
     }
   }
+
+# else
+  while (minu <= 48) {
+    int x = random(8);
+    int y = random(8);
+    if (get_pixel(x, y, HAUT) == OFF) {
+      set_pixel(x, y, HAUT, ON);
+      minu++;
+      draw_hourglass();
+      delay(10);
+    }
+  }
+
+
+
+# endif
+  Serial.println("====");
+  Serial.println((millis() - timer) / 1000);
+  timer = millis();
 }
 
 // cleanupp the lower glass slowly  :
 void empty_hourglass( int globe = BAS) {
- for (int i=0;i<=256;i++){
+  for (int i = 0; i <= 256; i++) {
     set_pixel(random(8), random(8), random(2), OFF);
-    animer(random(8), random(8) ,1);
+    animer(random(8), random(8) , 1);
     draw_hourglass();
     delay(10);
   }
+
 }
+
 // Routine to print the led array on the matrix
 void draw_hourglass() {
   for (int i = 0; i < 8; i++) {
@@ -95,10 +129,8 @@ void draw_hourglass() {
   matrix.write();
 }
 
-
 // Routine to move 1 pixel from X1/Y1 to X2/Y2 in the same globe :
-int move_grain(int x1, int y1, int x2, int y2, int globe)
-{
+int move_grain(int x1, int y1, int x2, int y2, int globe) {
   if (get_pixel(x2, y2, globe) == OFF) {   // there's no pixel here !
     set_pixel(x1, y1,   globe, OFF);
     set_pixel(x2, y2, globe, ON);
@@ -109,8 +141,7 @@ int move_grain(int x1, int y1, int x2, int y2, int globe)
 }
 
 // Routine to get the led's state :
-int get_pixel(int x, int y, int globe)
-{
+int get_pixel(int x, int y, int globe) {
   if ((x > 7) || (y > 7)) {
     return (255); // Outcote scope... sorry !
   }
@@ -123,12 +154,13 @@ int get_pixel(int x, int y, int globe)
 
 // Setting led to selected state :
 // X,Y,HAUT/BAS,ON/OFF
-void set_pixel(int x, int y, int globe, const int allume)
-{
+void set_pixel(int x, int y, int globe, const int allume) {
   if ((x > 7) || (y > 7)) {
     return;
   }
-
+  if ((x < 0) || (y < 0)) {
+    return;
+  }
   if (globe == BAS) {
     y += 8; // Offset for the lower matrix
   }
@@ -136,8 +168,7 @@ void set_pixel(int x, int y, int globe, const int allume)
 }
 
 // How pixels move (moving the pixel at X,Y from the HAUT/BAS globe)
-int animer(int x, int y, int globe)
-{
+int animer(int x, int y, int globe) {
   static int  cote = 0; // From the left ? To the right ...
   static int  gs = 0; // Number of grains that were NOT moved (fails)
 
@@ -197,8 +228,7 @@ int animer(int x, int y, int globe)
 }
 
 // Removing the last pixel of the upper globe, to the first of the lower :
-void drop()
-{
+void drop() {
   if (get_pixel(7, 7, HAUT) == OFF) {
     return;
   }
@@ -209,48 +239,102 @@ void drop()
   draw_hourglass();
 }
 
+// Function to draw integers on the screen
+void drawInt(int16_t x, int16_t y, int c,  byte globe) {
+  for (int8_t i = 0 ; i < 8; i++ ) {
+    uint8_t line;
+    line = pgm_read_byte(font + (i + 8 * c));
+    for (int8_t j = 0; j < 8; j++, line >>= 1 ) {
+      if (line & 0x1) {
+
+        set_pixel(x + j, y + 8 - i, globe, ON);
+      }
+    }
+  }
+}
+
+// Function tha display current time
+void displaytime() {
+  // Get current ime
+  DateTime now = rtc.now();
+  int ho = now.hour();
+  int mi = now.minute();
+  // Moving from out the screen to the center :
+  for (int k = 8; k >= 0; k--) {
+    cleanup_pixels();
+    drawInt(k, -k, ho / 10, HAUT);
+    drawInt(k, -k, ho - (ho / 10) * 10, BAS);
+    draw_hourglass();
+    delay(100);
+  }
+  delay(1000);
+  
+  // Moving the hours out & let the minutes come in :
+  for (int k = 0; k >= -8; k--) {
+    cleanup_pixels();
+    drawInt(k, -k, ho / 10, HAUT);
+    drawInt(k, -k, ho - (ho / 10) * 10, BAS);
+    drawInt(k + 8,  -k - 8, mi / 10, HAUT);
+    drawInt(k + 8,  -k - 8, mi - (mi / 10) * 10, BAS);
+    draw_hourglass();
+    delay(100);
+  }
+  delay(1000);
+  
+  // Push the minutes out :
+  for (int k = 0; k >= -8; k--) {
+    cleanup_pixels();
+    drawInt(k, -k, mi / 10, HAUT);
+    drawInt(k, -k, mi - (mi / 10) * 10, BAS);
+    draw_hourglass();
+    delay(100);
+  }
+
+}
 
 // Setu p :What elese ?
 void setup() {
   randomSeed(analogRead(A0)); // Initialize random generator
-  pinMode(12,INPUT); // SW350D Pinout
-  digitalWrite(12,HIGH); // Turnon Internal Pullup
-  
+  pinMode(12, INPUT); // SW350D Pinout
+  digitalWrite(12, HIGH); // Turnon Internal Pullup
   matrix.setIntensity(0); // Set brightness between 0 and 15
   matrix.setPosition(1, 0, 0); // The first display is at <0, 0>
   matrix.setPosition(0, 1, 0); // The second display is at <1, 0>
-  matrix.setRotation(0, 3);
+  matrix.setRotation(0, 3); // In case you glued the screens upside down
   matrix.setRotation(1, 1);
-  matrix.fillScreen(LOW);
-  ln = 5;
-  fill_hourglass(ln, HAUT);
-  startms = millis();
-}
+  matrix.fillScreen(LOW); // Turn of the screen
+  Wire.begin(); // Init SPI coms
+  rtc.begin(DateTime(F(__DATE__), F(__TIME__))); // Set date/time to software RTC
+  displaytime(); // To be sure ...
+  fill_hourglass(ln, HAUT); // First refill
+  startms = millis(); // init counter
+  }
 
+void reset_hourglass(){
+    empty_hourglass();
+    ln = random(8) + 1;
+    displaytime();
+    fill_hourglass(ln);
+    startms = now;
+    drop();
+  }
 
 // Main loop :
 void loop() {
-  // One second between each marble drop :
-  int htimeout = 1000;
-  long now = millis();
-  if ((now - startms) >= htimeout) {
-    startms = now;
+  // TIme since poweron :
+  long nowms = millis();
+  // timeout reached ?  Dropping 1 marble :
+  if ((nowms - startms) >= htimeout) {
+    startms = nowms;
     drop();
   }
-  if (digitalRead(12)==HIGH){ // If vibration recorded : reset !
-    empty_hourglass();
-    ln = random(8)+1;  
-    fill_hourglass(ln);
-    startms = now;
-    drop();
+  
+  if (digitalRead(12) == HIGH) { // If vibration recorded : reset !
+    reset_hourglass();
   }
+  
   // If we cannot manage to move 512 pixels, considering that the globe is empty.
   if (animer(random(8), random(8) , random(2) ) > 512) {
-    empty_hourglass();
-    ln = random(8)+1;  
-    fill_hourglass(ln);
-    startms = now;
-    drop();
-
+    reset_hourglass();
   }
 }
